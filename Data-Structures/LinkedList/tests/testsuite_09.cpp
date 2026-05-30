af9abf38f94ab55171500721c7229367236c43df
Zach Sally <zszach23@gmail.com>
2026-05-30 18:47:11 -0400

    [FIX] Update Append Test Suite

--- Data-Structures/LinkedList/tests/testsuite_09.cpp
+++ Data-Structures/LinkedList/tests/testsuite_09.cpp
@@ -25,8 +25,8 @@ TEST(LL_Append, AppendWithOneItemInList)
 
     list.Append(2);
 
-    EXPECT_EQ(list.Get(0), 2);
-    EXPECT_EQ(list.Get(1), 1);
+    EXPECT_EQ(list.Get(0), 1);
+    EXPECT_EQ(list.Get(1), 2);
 
     EXPECT_EQ(list.Size(), 2);
 }
@@ -38,12 +38,12 @@ TEST(LL_Append, AppendWithMultipleItemsInList)
 
     list.Append(100);
 
-    EXPECT_EQ(list.Get(0), 100);
-    EXPECT_EQ(list.Get(1), 1);
-    EXPECT_EQ(list.Get(2), 2);
-    EXPECT_EQ(list.Get(3), 3);
-    EXPECT_EQ(list.Get(4), 4);
-    EXPECT_EQ(list.Get(5), 5);
+    EXPECT_EQ(list.Get(0), 1);
+    EXPECT_EQ(list.Get(1), 2);
+    EXPECT_EQ(list.Get(2), 3);
+    EXPECT_EQ(list.Get(3), 4);
+    EXPECT_EQ(list.Get(4), 5);
+    EXPECT_EQ(list.Get(5), 100);
 
     EXPECT_EQ(list.Size(), 6);
 }
